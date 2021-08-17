f5dfece9c370c3ddc3e7fe1a456f38ca1875e32e
Leandre <leajapo@gmail.com>
2021-08-17 11:28:41 +0200

    feat:added user and order models

---
+++ models/order.js
@@ -0,0 +1,23 @@
+const { Types, model, Schema } = require('mongoose');
+
+const OrderSchema = new Schema(
+    {
+        userid: { type: Types.ObjectId, required: true },
+        products: [
+            {
+                id: { type: Number, required: true },
+                name: { type: String, required: true },
+                image: { type: String, required: true },
+                price: { type: Number, required: true },
+                brand: { type: String, required: true },
+                quantity: { type: Number, required: true },
+            },
+        ],
+        total: { type: Number, required: true },
+    },
+    { timestamps: true }
+);
+
+const Order = model('Order', OrderSchema);
+
+module.exports = Order;
