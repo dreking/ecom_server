5980519a2b7926f56e57510773478492f5017322
Leandre <leajapo@gmail.com>
2021-08-17 11:30:55 +0200

    feat: added auth controller logic, routes and validations

---
+++ validations/auth.js
@@ -0,0 +1,58 @@
+const { validationResult, body } = require('express-validator');
+
+const User = require('../models/user');
+
+exports.handleValidation = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+        return res.status(422).json({
+            status: false,
+            message: 'Validation error',
+            errors: errors.array(),
+        });
+    next();
+};
+
+exports.checkSignUp = [
+    body('username', 'Username is required')
+        .notEmpty()
+        .bail()
+        .isString()
+        .bail()
+        .trim()
+        .escape()
+        .customSanitizer((value) => value.replace(' ', ''))
+        .custom(async (value) => {
+            const user = await User.findOne({ username: value });
+            if (user) return Promise.reject('Username found');
+
+            return true;
+        }),
+    body('password', 'Password is required')
+        .notEmpty()
+        .bail()
+        .isString()
+        .bail()
+        .trim()
+        .isLength({ min: 5 })
+        .withMessage('Password must be at least 5 characters'),
+];
+
+exports.checkSignIn = [
+    body('username', 'Username is required')
+        .notEmpty()
+        .bail()
+        .isString()
+        .bail()
+        .trim()
+        .escape()
+        .customSanitizer((value) => value.replace(' ', '')),
+    body('password', 'Password is required')
+        .notEmpty()
+        .bail()
+        .isString()
+        .bail()
+        .trim()
+        .isLength({ min: 5 })
+        .withMessage('Password must be at least 5 characters'),
+];
